f5be16aeb224516a5c7f3f25b2126acc0fa3c58f
ramkj-gm <karamjabbour12@gmail.com>
2024-12-22 14:47:16 +0100

    [backend]: add list product

--- backend/src/products/index.js
+++ backend/src/products/index.js
@@ -2,4 +2,27 @@ import express from "express";
 import { connection } from "../db.js";
 
 const router = express.Router({});
+
+// list all products
+router.get("/", async (req, res) => {
+  try {
+    const { deleted } = req.query;
+    let condition = "";
+    let dbParams = [];
+    if(deleted != undefined){
+      condition = "where deleted = ?";
+      dbParams.push(deleted);
+    }
+    const [results] = await connection.query(
+      `SELECT * from articles ${condition}`,
+      dbParams
+    );
+    res.send({
+      data: results
+    });
+  } catch (e) {
+    res.status(500).send({ error: e });
+  }
+});
+
 export default router;
